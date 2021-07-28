038d4289d0c3d24d3474fe75ae76f76d23767e74
Quboid <github@quboid.net>
2021-07-28 03:25:21 +0100

    Initial commit

---
+++ LHNF/LHNF/Lifecycle.cs
@@ -0,0 +1,31 @@
+﻿using CitiesHarmony.API;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace LHNF
+{
+    class Lifecycle
+    {
+        public static void InstallMod()
+        {
+            Debug.Log($"LHNF Mod InstallMod");
+
+            HarmonyHelper.DoOnHarmonyReady(() => Patcher.PatchAll());
+
+            string msg = $"Assemblies:";
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                msg += $"\n{assembly.GetName().Name.ToLower()}";
+            }
+            Debug.Log(msg);
+        }
+
+        public static void UninstallMod()
+        {
+            Debug.Log($"LHNF Mod UninstallMod");
+
+            if (HarmonyHelper.IsHarmonyInstalled) Patcher.UnpatchAll();
+        }
+    }
+}
---
+++ LHNF/LHNF/ModInfo.cs
@@ -0,0 +1,65 @@
+﻿using ColossalFramework;
+using ColossalFramework.UI;
+using ICities;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LHNF
+{
+    public class ModInfo : LoadingExtensionBase, IUserMod
+    {
+        public string Name => "LHNF";
+        public string Description => "Left Hand Network Fix";
+
+        public ModInfo()
+        {
+        }
+
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            if (!(mode == LoadMode.LoadGame || mode == LoadMode.NewGame || mode == LoadMode.NewGameFromScenario || mode == LoadMode.LoadScenario || mode == LoadMode.NewMap || mode == LoadMode.LoadMap))
+            {
+                Debug.Log($"LHNF: Aborting because mode={mode}");
+                return;
+            }
+
+            Lifecycle.InstallMod();
+        }
+
+        public override void OnLevelUnloading()
+        {
+            Lifecycle.UninstallMod();
+        }
+
+        //public void OnSettingsUI(UIHelperBase helper)
+        //{
+        //    UIHelperBase group = helper.AddGroup(Name);
+
+        //    group.AddSpace(10);
+
+        //    ((UIPanel)((UIHelper)group).self).gameObject.AddComponent<OptionsKeymappingMain>();
+        //    UIPanel panel = ((UIHelper)group).self as UIPanel;
+
+        //    group.AddSpace(10);
+        //}
+
+        internal static bool InGame() => SceneManager.GetActiveScene().name == "Game";
+
+        public void OnEnabled()
+        {
+            if (InGame())
+            {
+                Lifecycle.InstallMod();
+            }
+        }
+
+        public void OnDisabled()
+        {
+            if (InGame())
+            {
+                Lifecycle.UninstallMod();
+            }
+        }
+    }
+}
